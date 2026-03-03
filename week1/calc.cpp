e3c777618c5ad3a77a30d6cb234c9cb7d317569b
Sangyae <bibekplus100@gmail.com>
2026-03-03 14:46:26 +0000

    nothing change just added some questions

---
+++ week1/calc.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+   int a, b;
+
+
+   cout << "Enter the first integer: "; 
+
+   cin >> a;
+
+   cout << "Enter the second integer: ";
+
+   cin >> b;
+
+   cout << "Addition (a + b): " << (a + b) << endl;
+
+   cout << "Subtraction (a - b): " << (a - b) << endl;
+
+   cout << "Multiplication (a * b): " << (a * b) << endl;
+
+   cout << "Division (a / b): " << (a / b) << endl;
+
+   cout << "Modulo (a % b): " << (a % b) << endl;
+
+ 
+
+   return 0;
+
+}
+
