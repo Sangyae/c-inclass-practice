5e89c29bba2e98b4fbb3be7f10c7e425bf8851b7
Sangyae <bibekplus100@gmail.com>
2026-03-05 22:30:45 +0000

    testing for hackathon

--- week1/week4/inclass.cpp
+++ week1/week4/inclass.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
     int main() {    
         int t[] = { 1, 2 };
         int* pt = &(t[0]);
             for (int i = 0; i < 2; i++) {
-                std::cout << "t[" << i << "]: " << t[i] << std::endl;
-                std::cout << "pt + " << i << ": " << *(pt + i) <<std::endl;
+                cout << "t[" << i << "]: " << t[i] << endl;
+                cout << "pt + " << i << ": " << *(pt + i) <<endl;
             }
 }
